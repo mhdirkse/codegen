e47358c50162fc65582ef1566f6d2e831d577a57
Martijn Dirkse <mhdirkse@live.nl>
2018-05-19 15:17:38 +0200

    Combined existing projects in this git project with pom composition

---
+++ codegenPlugin/src/main/java/com/github/mhdirkse/refplug/MyMojo.java
@@ -0,0 +1,92 @@
+package com.github.mhdirkse.refplug;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.descriptor.PluginDescriptor;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.classworlds.realm.ClassRealm;
+
+/**
+ * Goal which generates .java files from POJO description files.
+ */
+@Mojo(
+  name = "reflection",
+  defaultPhase = LifecyclePhase.GENERATE_SOURCES,
+  requiresDependencyResolution = ResolutionScope.COMPILE,
+  requiresProject = true)
+public class MyMojo extends AbstractMojo
+{
+    @Component
+    private MavenProject project;
+
+    @Component
+    private PluginDescriptor descriptor;
+
+    public void execute()
+        throws MojoExecutionException
+    {
+        List<String> runtimeClasspathElements = getClasspathElements();
+        ClassRealm realm = descriptor.getClassRealm();
+
+        for (String element : runtimeClasspathElements)
+        {
+            File elementFile = new File(element);
+            try {
+                URL url = elementFile.toURI().toURL();
+                System.out.println("Adding classpath element: " + url.toString());
+                realm.addURL(url);
+            } catch(MalformedURLException e) {
+                throw new MojoExecutionException(
+                        "Malformed URL for file " + elementFile.toString(), e);
+            }
+        }
+        Class<?> myInterfaceClazz;
+        try {
+            myInterfaceClazz = realm.loadClass("com.github.mhdirkse.simpledep.MyInterface");
+        } catch(ClassNotFoundException e) {
+            throw new MojoExecutionException("Class not found");
+        }
+
+        Method[] methods = myInterfaceClazz.getMethods();
+        for (Method method: methods) {
+            System.out.println("Found method: " + method.getName());
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<String> getClasspathElements() throws MojoExecutionException {
+        List<String> runtimeClasspathElements;
+        try {
+            runtimeClasspathElements = project.getRuntimeClasspathElements();
+        } catch(Exception e) {
+            throw new MojoExecutionException("Could not get dependencies", e);
+        }
+        return runtimeClasspathElements;
+    }
+}
---
+++ codegenTestInput/src/main/java/com/github/mhdirkse/simpledep/MyInterface.java
@@ -0,0 +1,6 @@
+package com.github.mhdirkse.simpledep;
+
+public interface MyInterface {
+    void firstMethod();
+    void secondMethod();
+}
