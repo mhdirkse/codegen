63e606a5fbf4e50c5b577a305409f30b7e599386
Martijn Dirkse <mhdirkse@live.nl>
2018-05-21 19:44:02 +0200

    Last commit was not complete, extend

--- codegenPlugin/src/main/java/com/github/mhdirkse/codegen/plugin/Task.java
+++ codegenPlugin/src/main/java/com/github/mhdirkse/codegen/plugin/Task.java
@@ -4,6 +4,7 @@ public class Task {
     private String source;
     private String handler;
     private String abstractHandler;
+    private String delegator;
 
     public String getSource() {
         return source;
@@ -27,5 +28,13 @@ public class Task {
 
     public final void setAbstractHandler(String abstractHandler) {
         this.abstractHandler = abstractHandler;
+    }
+
+    public final String getDelegator() {
+        return delegator;
+    }
+
+    public final void setDelegator(String delegator) {
+        this.delegator = delegator;
     }
 }
--- codegenPlugin/src/main/java/com/github/mhdirkse/codegen/plugin/VelocityContextMethod.java
+++ codegenPlugin/src/main/java/com/github/mhdirkse/codegen/plugin/VelocityContextMethod.java
@@ -9,24 +9,67 @@ import org.apache.commons.lang.StringUtils;
 public class VelocityContextMethod {
     private String name;
     private String signature;
+    private String signatureForHandler;
+    private String finalSignature;
+    private String finalSignatureForHandler;
+    private String actualParameters;
+    private String actualParametersForHandler;
 
-    public VelocityContextMethod(final Method reflectionMethod) {
+    public VelocityContextMethod(final Method reflectionMethod, final String handlerStackTypeParameter) {
         name = reflectionMethod.getName();
         List<String> parameters = new ArrayList<>();
+        List<String> finalParameters = new ArrayList<>();
         int seq = 0;
         for (Class<?> reflectionParameterType : reflectionMethod.getParameterTypes()) {
-            parameters.add(
-                    String.format("%s p%d",
-                            reflectionParameterType.getCanonicalName(),
-                            ++seq));
+            String simpleParameterSignature = String.format("%s p%d",
+                    reflectionParameterType.getCanonicalName(), ++seq);
+            parameters.add(simpleParameterSignature);
+            finalParameters.add("final " + simpleParameterSignature);
         }
         signature = StringUtils.join(parameters, ", ");
+        finalSignature = StringUtils.join(finalParameters, ", ");
+        String simpleContextParameterSignature =
+                String.format("com.github.mhdirkse.codegen.runtime.HandlerStackContext<%s> ctx",
+                        handlerStackTypeParameter);
+        parameters.add(simpleContextParameterSignature);
+        finalParameters.add("final " + simpleContextParameterSignature);
+        signatureForHandler = StringUtils.join(parameters, ",");
+        finalSignatureForHandler = StringUtils.join(finalParameters, ", ");
+
+        List<String> actualParametersList = new ArrayList<>();
+        for(int i = 1; i <= seq; ++i) {
+            actualParametersList.add("p" + new Integer(i).toString());
+        }
+        actualParameters = StringUtils.join(actualParametersList, ", ");
+        actualParametersList.add("ctx");
+        actualParametersForHandler = StringUtils.join(actualParametersList, ", ");
     }
 
     public final String getName() {
         return name;
     }
+
     public final String getSignature() {
         return signature;
+    }
+
+    public final String getSignatureForHandler() {
+        return signatureForHandler;
+    }
+
+    public final String getFinalSignature() {
+        return finalSignature;
+    }
+
+    public final String getFinalSignatureForHandler() {
+        return finalSignatureForHandler;
+    }
+
+    public final String getActualParameters() {
+        return actualParameters;
+    }
+
+    public final String getActualParametersForHandler() {
+        return actualParametersForHandler;
     }
 }
