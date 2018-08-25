c4e9f1287f6e5572e1da450702f04251e30ac70f
martijn <mhdirkse@live.nl>
2018-08-25 16:34:26 +0200

    Remove unused status code.

--- codegenPlugin/src/main/java/com/github/mhdirkse/codegen/plugin/impl/StatusCode.java
+++ codegenPlugin/src/main/java/com/github/mhdirkse/codegen/plugin/impl/StatusCode.java
@@ -17,7 +17,6 @@ enum StatusCode {
     FIELD_GET_ERROR_AFTER_PROGRAM_RUN(
             "@{1} field {2} was not available after running your Codegen program. It may have cleared the field."),
     FIELD_REQUIRED_CLASS_NOT_FOUND("@{1} field {2} requires class {3}, but it is not in your dependencies."),
-    FIELD_NOT_VELOCITY_CONTEXT("@{1} field {2} is not a VelocityContext."),
     FIELD_VELOCITY_CONTEXT_LACKS_TARGET("@{1} field {2}: VelocityContext does not have key \"target\"."),
     FIELD_VELOCITY_CONTEXT_TARGET_NOT_CLASS_MODEL(
             "@{1} field {2}: The \"target\" should be a {3}, but was {4}."),
