d7625214f48086802e3cd8699386bdce18036347
Martijn Dirkse <martijn@integrationpartners.nl>
2020-12-01 18:48:42 +0100

    Fix build for Windows

--- codegenPlugin/src/test/java/com/github/mhdirkse/codegen/plugin/CodegenMojoTest.java
+++ codegenPlugin/src/test/java/com/github/mhdirkse/codegen/plugin/CodegenMojoTest.java
@@ -8,13 +8,21 @@ import org.junit.Test;
 public class CodegenMojoTest {
     @Test
     public void testPackageToRelativePathLinux() {
-        Assert.assertEquals("base/com/github/mhdirkse/X.java", CodegenMojo.classToPathOfJavaFile(
+        Assert.assertEquals(adjustForOS("base/com/github/mhdirkse/X.java"), CodegenMojo.classToPathOfJavaFile(
                 new File("base"), "com.github.mhdirkse.X").toString());
+    }
+
+    private static String adjustForOS(String linuxPath) {
+        String replacement = "\\\\";
+        if(File.separator.equals("/")) {
+            replacement = "/";
+        }
+        return linuxPath.replaceAll("/", replacement);
     }
 
     @Test
     public void testPackageToRelativePathLinuxNoPath() {
-        Assert.assertEquals("base/X.java", CodegenMojo.classToPathOfJavaFile(
+        Assert.assertEquals(adjustForOS("base/X.java"), CodegenMojo.classToPathOfJavaFile(
                 new File("base"), "X").toString());
     }
 }
